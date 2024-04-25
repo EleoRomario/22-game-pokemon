5d99ec9c234cafa159a0f44100e6b7e9a14e5d28
EleoRomario <eleo.romario@gmail.com>
2024-04-25 17:40:12 -0500

    Actualizar configuración de Tailwind CSS para agregar animación de error

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ export default {
 			colors: {
 				accent: "#5f37bc",
 			},
+			animation: {
+				error: "error 0.5s ease-in-out",
+			},
+			keyframes: {
+				error: {
+					"0%, 100%": {
+						transform: "translateX(0)",
+					},
+					"25%, 75%": {
+						transform: "translateX(-5px)",
+					},
+					"50%": {
+						transform: "translateX(5px)",
+					},
+				},
+			},
 		},
 	},
 	plugins: [],
