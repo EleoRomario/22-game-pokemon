13f048d9868840ba2aac43d593bd16a71dc17165
EleoRomario <eleo.romario@gmail.com>
2024-04-25 17:41:01 -0500

    Actualizar componente Alternatives para cambiar el orden de las alternativas y añadir prop onClick

--- src/components/common/Alternatives.tsx
+++ src/components/common/Alternatives.tsx
@@ -1,24 +1,27 @@
-import { Pokeball } from "../../icons";
+import { useState } from "react";
+import { PokeballAlternative } from "../../icons";
 import { ANSWER } from "../../types/types";
 import { Button } from "./Button";
-import confetti from "canvas-confetti";
 
-export const Alternatives = ({ alternatives }: { alternatives: ANSWER[] }) => {
-	const handleCorrect = (isCorrect: boolean) => {
-		if (isCorrect) {
-			confetti();
-		} else {
-			console.log("Incorrecto");
-		}
-	};
+interface Alternatives {
+	alternatives: ANSWER[];
+	onClick: (isCorrect: boolean) => void;
+}
+
+export const Alternatives = ({ alternatives, onClick }: Alternatives) => {
+	const [changeOrderAlternatives] = useState(() => {
+		return alternatives.sort(() => Math.random() - 0.5);
+	});
+
 	return (
-		<ul className="flex gap-4 flex-col">
-			{alternatives.map((alternative, index) => (
+		<ul className="flex gap-4 flex-col w-full">
+			{changeOrderAlternatives.map((alternative, index) => (
 				<li key={index}>
 					<Button
 						label={alternative.name}
-						icon={Pokeball}
-						onClick={() => handleCorrect(alternative.isCorrect)}
+						icon={PokeballAlternative}
+						onClick={() => onClick(alternative.isCorrect)}
+						className="w-full"
 					/>
 				</li>
 			))}
