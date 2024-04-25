7aa48a183ccbc08b0fb0e3ecf93832ae4065703b
EleoRomario <eleo.romario@gmail.com>
2024-04-25 13:59:02 -0500

    Añadir componente TagTypePokemon y funciones relacionadas

---
+++ src/components/TagTypePokemon.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { getType } from "../helpers/getType";
+import { POKEMON_TYPES } from "../types/types";
+
+interface TAG{
+  type: POKEMON_TYPES[],
+  typeEs: string[]
+}
+export const TagTypePokemon = ({type,typeEs}:TAG) => {
+
+  const [types, setTypes] = useState<{color:string,typeEs:string, icon:JSX.Element | undefined}[]>([]);
+
+  useEffect(() => {
+    const typeFormat = () => {
+      return type.map((type,index) => {
+        const typePokemon = getType(type)
+        return {...typePokemon, typeEs: typeEs[index]}      
+      })
+    }
+    const format = typeFormat()
+    setTypes(format)
+
+  }, [type,typeEs])
+
+  return (
+    <div>
+      {types.map(({color, icon},index) => (
+        <span key={index} className="flex justify-center items-center gap-1 text-white rounded-lg px-2 py-1 text-xs font-semibold mr-2" style={{
+          color,
+          border: `1px solid ${color}`
+        }}>
+          {icon}
+          {typeEs}
+        </span>
+      ))}
+    </div>
+  )
+}
