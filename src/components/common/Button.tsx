c87919c3bfb25607bf829b1028b117d8f1e78139
EleoRomario <eleo.romario@gmail.com>
2024-04-25 17:40:52 -0500

    Actualizar estilos del componente Button y añadir prop className

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,12 +2,18 @@ interface ButtonProps {
 	label: string;
 	icon: React.FC;
 	onClick: () => void;
+	className?: string;
 }
-export const Button = ({ label, onClick, icon: Icon }: ButtonProps) => {
+export const Button = ({
+	label,
+	onClick,
+	icon: Icon,
+	className,
+}: ButtonProps) => {
 	return (
 		<button
 			onClick={onClick}
-			className="py-4 px-8 rounded-full border flex gap-2 uppercase hover:bg-accent/20  transition duration-300 ease-in-out hover:scale-105"
+			className={`py-4 px-8 rounded-full border flex gap-2 uppercase hover:bg-accent/20  transition duration-300 ease-in-out hover:scale-105 ${className}`}
 		>
 			<Icon />
 			{label}
