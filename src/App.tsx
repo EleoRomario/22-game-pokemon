2b8299c1706904b7975053d32512d8f346cdac00
EleoRomario <eleo.romario@gmail.com>
2024-04-26 09:12:49 -0500

    Agregar componente Particles.tsx para animación de partículas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Particles } from "./animations/Particles";
 import { Game } from "./components/interfaces/Game";
 import { MenuInitial } from "./components/interfaces/MenuInitial";
 import { useGame } from "./store/game";
@@ -7,6 +8,7 @@ function App() {
 	const gameInit = useGame((state) => state.gameInit);
 	return (
 		<main className="relative bg-[#0d1a35] flex justify-center text-white min-h-screen p-20 items-center">
+			<Particles className="absolute size-full" quantity={80} />
 			<div className="z-0 fixed w-96 h-96 bg-accent blur-[150px] rounded-full"></div>
 			<div className="z-10">{!gameInit ? <MenuInitial /> : <Game />}</div>
 		</main>
