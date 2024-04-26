2fbe74644898047a408f383fb7029894b9eec0f5
EleoRomario <eleo.romario@gmail.com>
2024-04-26 10:23:28 -0500

    Actualizar tamaño de partículas en componente Particles.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 	const gameInit = useGame((state) => state.gameInit);
 	return (
 		<main className="relative bg-[#0d1a35] flex justify-center text-white min-h-screen p-20 items-center">
-			<Particles className="absolute size-full" quantity={80} />
+			<Particles className="absolute size-[80%]" quantity={80} />
 			<div className="z-0 fixed w-96 h-96 bg-accent blur-[150px] rounded-full"></div>
 			<div className="z-10">{!gameInit ? <MenuInitial /> : <Game />}</div>
 		</main>
