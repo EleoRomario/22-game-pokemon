946a575586178c63608c2cb3b242bb3842f3ce03
EleoRomario <eleo.romario@gmail.com>
2024-04-26 10:47:05 -0500

    Actualizar componente CardQuestion.tsx para mostrar animación de carga y manejar errores

--- src/components/common/CardQuestion.tsx
+++ src/components/common/CardQuestion.tsx
@@ -2,6 +2,8 @@ import confetti from "canvas-confetti";
 import { usePokemon } from "../../hooks/usePokemon";
 import { Alternatives } from "./Alternatives";
 import { useEffect, useState } from "react";
+import { CardContainer } from "./CardContainer";
+import { LoadingPokemon } from "../../animations/LoadingPokemon";
 
 export const CardQuestion = () => {
 	const { pokemon, alternatives, isLoading, error } = usePokemon();
@@ -22,37 +24,28 @@ export const CardQuestion = () => {
 		}
 	};
 
+	if (isLoading) return <LoadingPokemon />;
+	if (error) return <div>Error</div>;
+
 	return (
-		<div
-			className={`${
-				!alternativeCorrect && "animate-error"
-			} w-96 h-auto bg-[#111111] relative rounded-3xl flex `}
-		>
-			<div
-				className="absolute top-0 left-0 bottom-0 right-0 rounded-3xl z-0"
-				style={{
-					backgroundSize: "100% 100%",
-					backgroundPosition: "0px 0px",
-					backgroundImage: `radial-gradient(142% 91% at 83% 7%, ${pokemon?.color}20 1%, #FF000000 99%)`,
-				}}
-			></div>
-			{isLoading && <p>Loading...</p>}
-			{error && <p>Error...</p>}
+		<>
 			{pokemon && (
-				<div className="flex text-white flex-col items-center gap-4 p-10 z-10">
-					<div className="h-60 w-full flex justify-center items-end">
-						<img
-							src={pokemon.img}
-							alt={pokemon.name}
-							className="h-80 aspect-square"
+				<CardContainer className="" pokemon={pokemon}>
+					<div className="flex text-white flex-col items-center gap-4 p-10 z-10">
+						<div className="h-60 w-full flex justify-center items-end">
+							<img
+								src={pokemon.img}
+								alt={pokemon.name}
+								className="h-80 aspect-square"
+							/>
+						</div>
+						<Alternatives
+							alternatives={alternatives}
+							onClick={handleCorrect}
 						/>
 					</div>
-					<Alternatives
-						alternatives={alternatives}
-						onClick={handleCorrect}
-					/>
-				</div>
+				</CardContainer>
 			)}
-		</div>
+		</>
 	);
 };
