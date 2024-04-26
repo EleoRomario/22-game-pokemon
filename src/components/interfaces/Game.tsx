79cb8e7263be39ea8c788a623417bebe5816e354
EleoRomario <eleo.romario@gmail.com>
2024-04-26 13:56:19 -0500

    Actualizar componente Game.tsx para agregar el componente HeaderQuestion en la interfaz

--- src/components/interfaces/Game.tsx
+++ src/components/interfaces/Game.tsx
@@ -1,9 +1,11 @@
 import { Logo } from "../../icons";
 import { CardQuestion } from "../common/CardQuestion";
+import { HeaderQuestion } from "../common/HeaderQuestion";
 
 export const Game = () => {
 	return (
 		<section className="flex flex-col items-center gap-10">
+			<HeaderQuestion />
 			<h1 className="flex gap-2 text-5xl items-center">
 				<span>¿Qué</span>
 				<span>
