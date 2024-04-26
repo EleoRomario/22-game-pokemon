6133f43fb7119cc854e578f1031442baa2b18e76
EleoRomario <eleo.romario@gmail.com>
2024-04-26 08:17:17 -0500

    Actualizar estilos del componente App.tsx y estilos relacionados

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import { useGame } from "./store/game";
 function App() {
 	const gameInit = useGame((state) => state.gameInit);
 	return (
-		<main className="relative flex justify-center text-white min-h-screen p-20 items-center">
-			<div className="layout -z-10 fixed top-0 left-0 right-0 bottom-0"></div>
-			{!gameInit ? <MenuInitial /> : <Game />}
+		<main className="relative bg-[#0d1a35] flex justify-center text-white min-h-screen p-20 items-center">
+			<div className="z-0 fixed w-96 h-96 bg-accent blur-[150px] rounded-full"></div>
+			<div className="z-10">{!gameInit ? <MenuInitial /> : <Game />}</div>
 		</main>
 	);
 }
