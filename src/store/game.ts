94c3e51318c90b4433c287a5ffa30cb431283231
EleoRomario <eleo.romario@gmail.com>
2024-04-26 13:55:43 -0500

    Actualizar componente game.ts para agregar la propiedad inGame al estado del juego

--- src/store/game.ts
+++ src/store/game.ts
@@ -3,13 +3,17 @@ import { create } from "zustand";
 interface GameState {
 	gameInit: boolean;
 	color: string;
+	inGame: boolean;
 	setGameInit: (value: boolean) => void;
 	setColor: (value: string) => void;
+	setInGame: (value: boolean) => void;
 }
 
 export const useGame = create<GameState>((set) => ({
 	gameInit: false,
 	color: "#364eba",
+	inGame: false,
 	setGameInit: (value) => set({ gameInit: value }),
 	setColor: (value) => set({ color: value }),
+	setInGame: (value) => set({ inGame: value }),
 }));
