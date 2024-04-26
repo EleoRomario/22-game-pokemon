d8911e13e31eb97ba135bc44028381efc39ddc61
EleoRomario <eleo.romario@gmail.com>
2024-04-26 13:30:51 -0500

    Actualizar componente Light.tsx para agregar animación de luz

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Light } from "./animations/Light";
 import { Particles } from "./animations/Particles";
 import { Game } from "./components/interfaces/Game";
 import { MenuInitial } from "./components/interfaces/MenuInitial";
@@ -6,10 +7,17 @@ import { useGame } from "./store/game";
 
 function App() {
 	const gameInit = useGame((state) => state.gameInit);
+	const color = useGame((state) => state.color);
 	return (
-		<main className="relative bg-[#0d1a35] flex justify-center text-white min-h-screen p-20 items-center">
+		<main className="relative bg-bg flex justify-center text-white min-h-screen p-20 items-center">
+			<Light />
 			<Particles className="absolute size-[80%]" quantity={80} />
-			<div className="z-0 fixed w-96 h-96 bg-accent blur-[150px] rounded-full"></div>
+			<div
+				className="z-0 fixed w-96 h-96 bg-bg blur-[150px] rounded-full"
+				style={{
+					backgroundColor: color.concat("80"),
+				}}
+			></div>
 			<div className="z-10">{!gameInit ? <MenuInitial /> : <Game />}</div>
 		</main>
 	);
