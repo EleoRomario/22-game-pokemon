250e601ce009c80f45d961c232ad3a71f232760a
EleoRomario <eleo.romario@gmail.com>
2024-04-26 10:35:06 -0500

    Actualizar función getPokemonsAlternatives para obtener alternativas de Pokémon en getPokemonsAlternatives.ts

--- src/helpers/getPokemonsAlternatives.ts
+++ src/helpers/getPokemonsAlternatives.ts
@@ -1,14 +1,14 @@
-import { ANSWER, POKEMON } from "../types/types";
+import { ANSWER } from "../types/types";
 
-export function getPokemonsAlternatives(pokemons: POKEMON[]) {
-	const pokemon = pokemons[Math.floor(Math.random() * pokemons.length)].name;
+export function getPokemonsAlternatives(pokemons: string[]) {
+	const pokemon = pokemons[Math.floor(Math.random() * pokemons.length)];
 
-	const alternatives = pokemons.filter((p) => p.name !== pokemon);
+	const alternatives = pokemons.filter((p) => p !== pokemon);
 	const randomAlternatives = alternatives.sort(() => Math.random() - 0.5);
 
 	const answerAlternatives = randomAlternatives.slice(0, 3).map((p) => {
 		return {
-			name: p.name,
+			name: p,
 			isCorrect: false,
 		};
 	});
