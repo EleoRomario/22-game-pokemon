4ff0b4de251a59e6da3e8d34dedbf969d89a1f28
EleoRomario <eleo.romario@gmail.com>
2024-04-26 15:02:25 -0500

    Actualizar componente ButtonExit.tsx para usar el componente ButtonUI en lugar de Button

--- src/components/common/ButtonExit.tsx
+++ src/components/common/ButtonExit.tsx
@@ -1,6 +1,6 @@
 import { Exit } from "../../icons";
 import { useGame } from "../../store/game";
-import { Button } from "./Button";
+import { ButtonUI } from "./ButtonUI";
 
 export const ButtonExit = () => {
 	const setInGame = useGame((state) => state.setInGame);
@@ -9,5 +9,12 @@ export const ButtonExit = () => {
 		setInGame(false);
 	};
 
-	return <Button icon={Exit} label="Salir" onClick={handleExit}></Button>;
+	return (
+		<ButtonUI
+			icon={Exit}
+			label=""
+			onClick={handleExit}
+			className="py-1 px-2"
+		></ButtonUI>
+	);
 };
---
+++ src/components/common/ButtonUI.tsx
@@ -0,0 +1,22 @@
+interface ButtonProps {
+	label: string;
+	icon: React.FC;
+	onClick: () => void;
+	className?: string;
+}
+export const ButtonUI = ({
+	label,
+	onClick,
+	icon: Icon,
+	className,
+}: ButtonProps) => {
+	return (
+		<button
+			onClick={onClick}
+			className={`px-2 py-2 rounded-lg flex gap-2 uppercase bg-gray-500/10 hover:bg-gray-500/20  transition duration-300 items-center ease-in-out hover:scale-105 ${className}`}
+		>
+			<Icon />
+			{label}
+		</button>
+	);
+};
