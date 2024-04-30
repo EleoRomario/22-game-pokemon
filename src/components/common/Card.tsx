0f1be2eada49a80a2afaad47a3b5d4d172f112b4
EleoRomario <eleo.romario@gmail.com>
2024-04-30 09:38:05 -0500

    Actualizar estilos de imagen en Card.tsx y eliminar import innecesario en Game.tsx

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -3,12 +3,12 @@ export const Card = () => {
 	return (
 		<div className="animate-zoom-in relative">
 			<img
-				src="../../../public/portada.svg"
+				src="/portada.svg"
 				className="absolute opacity-[0.01] -z-10 rounded-3xl scale-[500%] brightness-200"
 			></img>
 			<Bubble color="#f2d94e" className="size-40">
 				<img
-					src="../../../public/portada.svg"
+					src="/portada.svg"
 					alt="Juego adivina el pokemon"
 					className="h-20 z-20"
 				/>
